f0e185b6aad5973978278d960b48984c30916328
Michael Marsh <mike@marsh.pw>
2026-05-23 16:07:09 -0400

    tweaks to custom component

--- src/skill_match/views/components.cljs
+++ src/skill_match/views/components.cljs
@@ -8,7 +8,12 @@
 
 (def ^:private $ jQuery)
 
-
+;; obstacles to generalization in separate lib
+;; * jQuery (not really, they'll just have deal with it for now)
+;; * input applies class names rather than colors directly (maybe this can be fixed?)
+;;   * can "jquery up" some <style> tags lol
+;; * requires unique dom id per element
+;;   * either needs descriptive exceptions or perhaps theres a way to go reagent -> React -> raw dom
 (defn highlightable-textarea [{:keys [highlights]
                                :or {highlights (r/atom true)}
                                :as props} & body]
@@ -22,14 +27,15 @@
             (.highlightWithinTextarea #js {:highlight #(clj->js @current-highlights)})))
       
       :component-did-update 
-      (fn [_] 
-        (-> ($ dom-id) (.highlightWithinTextarea "update")))
+      (fn [_]  (-> ($ dom-id) (.highlightWithinTextarea "update")))
+      
+      :component-will-unmount
+      (fn [_] (-> ($ dom-id) (.highlightWithinTextarea "destroy")))
       
       :reagent-render
-      (fn []
-        [:div
-         (apply vector :textarea
-                (-> props
-                    (assoc (str "data-" (update-highlights @highlights)) true)
-                    (dissoc :highlights))
-                body)])})))
+      (fn [] 
+        (apply vector :textarea
+               (-> props
+                   (assoc "data-current-highlighting" (update-highlights @highlights))
+                   (dissoc :highlights))
+               body))})))
