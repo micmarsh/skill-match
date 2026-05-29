49a2ab690c4a22f7b4ce9235aa3d54bbcc7ec4c5
Michael Marsh <mike@marsh.pw>
2026-05-29 12:24:45 -0400

    move non-http stuff out for some breathing room

--- pdf_api/main.py
+++ pdf_api/main.py
@@ -1,24 +1,21 @@
 
 from fastapi import FastAPI, Response
 from pydantic import BaseModel
-from weasyprint import HTML
 from typing import List
+from utilities import render_template, weasyprint_render
 
 app = FastAPI()
-
-def weasyprint_render(html: str):
-    return HTML(string=html).render().write_pdf()
 
 class HtmlRender(BaseModel):
     body: str
 
 #TODO maybe dynamic filename based on hash of content or something or other?
-headers = {"Content-Disposition": "inline; filename=Rendered.pdf"}
+pdf_headers = {"Content-Disposition": "inline; filename=Rendered.pdf"}
 
 @app.post("/render_raw")
 async def render_pdf(html: HtmlRender):
     pdf_bytes = weasyprint_render(html.body)
-    return Response(pdf_bytes, media_type="application/pdf", headers=headers)
+    return Response(pdf_bytes, media_type="application/pdf", headers=pdf_headers)
 
 class ResumeRender(BaseModel):
     skills: List[str]
@@ -30,14 +27,6 @@ with open("/home/michael/Documents/JobSearch/Resume_Template.html") as f:
 
 @app.post("render_resume")
 async def render_resume(input: ResumeRender):
-    
-
-
-# TODO probably delete these, just for playing around
-@app.get("/")
-async def read_root():
-    return {"message": "Hello, Dawg!"}
-
-@app.get("/square")
-async def calculate_square(num: int):
-    return {"numba": num, "square": num ** 2}
+    full_resume_text = render_template(template, input.skills)
+    pdf_bytes = weasyprint_render(html.body)
+    return Response(pdf_bytes, media_type="application/pdf", headers=pdf_headers)
